1211f210382c94e16483a24ab972e8cc37c50dac
Aluno ProWay <aluno@proway.treina>
2024-07-15 21:17:21 -0300

    Trabalhando com rotas com outlet

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider} from 'react-router-dom'
+
 import Home from './pages/home';
 import CadUsuario from './pages/cadUsuario';
 import CadContato from './pages/cadContato';
@@ -7,20 +9,42 @@ import ConsultaContatos from './pages/consultaContatos';
 import Info from './pages/info';
 import Login from './pages/login';
 import EditaContato from './pages/editaContato';
+import { useState } from 'react';
+
+const routes = createBrowserRouter([
+  {
+    path:'', element:<Home />,
+    children:[
+      { path:'usuario/registrar', element: <CadUsuario /> }, 
+      { path:'usuario/login', element:<Login /> },
+      { path:'contato/cadastrar', element: <CadContato />},
+      { path:'contato/consultar', element: <ConsultaContatos />},
+      { path:'contato/consultar/:id', element: <EditaContato />},
+      { path:'*', element:<Info />}            
+    ]
+  }
+])
 
 function App() {
+  const [isLogged, setIslogged] = useState(localStorage.getItem('userLogado') != null)
+   
   return (
-    <BrowserRouter>
+   /* <BrowserRouter>
         <Routes>
            <Route path='' element={<Home /> } />
            <Route path='usuario/registrar' element={<CadUsuario />} />
            <Route path='usuario/login' element={<Login />} />
-           <Route path='contato/cadastrar' element={<CadContato />}/>
+          
+           <Route path='contato/cadastrar' element={<CadUsuario />}/>
            <Route path='contato/consultar' element={<ConsultaContatos />}/>
+
            <Route path='contato/consultar/:id' element={<EditaContato />}/>
            <Route path='*' element={<Info />} />
         </Routes>
-    </BrowserRouter>
+    </BrowserRouter>*/
+    <>      
+      <RouterProvider router={routes}/>
+    </>
   );
 }
 
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,19 +1,33 @@
+import { useEffect, useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 
-export default function Home(){
-    return(
+export default function Home() {
+    const [isLogged, setIslogged] = useState<boolean>()
+    useEffect(()=>{
+        setIslogged(localStorage.getItem('userLogado') != null)
+
+    },[isLogged])
+    
+    return (
         <div>
             <nav>
                 <ul>
                     <li><Link to='/'>Home</Link></li>
                     <li><Link to='/usuario/registrar'>Novo usuario</Link></li>
                     <li><Link to='/usuario/login'>Login</Link></li>
-                    <li><Link to='/contato/cadastrar'>Novo contato</Link></li>
-                    <li><Link to='/contato/consultar'>listar contatos</Link></li>
+                    {
+                        /**renderização condicional */
+                        isLogged ? <>
+                            <li><Link to='/contato/cadastrar'>Novo contato</Link></li>
+                            <li><Link to='/contato/consultar'>listar contatos</Link></li>  
+                            </>
+                            : ""       
+                            
+                    }
                 </ul>
             </nav>
             <main>
-               
+               <Outlet />
             </main>
         </div>
     )
